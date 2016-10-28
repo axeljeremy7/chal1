88c1b44ca829b6d4bd97d3355e5a063efcebdf21
Axel Jeremy Solano <axel.jeremy.7@gmail.com>
2016-10-28 15:46:11 -0400

    Update Stepmain.java

--- Stepmain.java
+++ Stepmain.java
@@ -62,6 +62,43 @@ public class Stepmain {
 			System.out.println(response.toString());
 
 		}
+	
+	private void sendPosStep2t() throws Exception {
+
+			
+			String url = "http://challenge.code2040.org/api/reverse/validate";
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			//add reuqest header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+			//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+			String urlParameters = "token=625e546a20c2f6d4b7dabe8be841a23e&string="+strtoreturn;
+
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(urlParameters);
+			wr.flush();
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			
+
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			System.out.println(response.toString());
+
+		}
 		
 		private void reverseWord(String str){
 			char [] ar = str.toCharArray();
