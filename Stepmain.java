ab21e5b7b665b7c6b75f173d675f67f8f5c2ff19
Axel Jeremy Solano <axel.jeremy.7@gmail.com>
2016-10-28 15:37:08 -0400

    Update Stepmain.java

--- Stepmain.java
+++ Stepmain.java
@@ -14,64 +14,31 @@ public class Stepmain {
 		// TODO Auto-generated method stub
 		Stepmain http = new Stepmain();
 
-		//System.out.println("Testing 1 - Send Http GET request");
-		//http.sendGet();
+		
 
-		System.out.println("\nTesting 2 - Send Http POST request");
+		System.out.println("\nSend Http POST request");
 		//http.sendPost();
-		http.reverseWord("MESSI is the best");
+		//http.reverseWord("MESSI is the best");
 
 
 	}
 	
-	private void sendGet() throws Exception {
-
-		String url = "http://challenge.code2040.org/api/register";
-
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		// optional default is GET
-		con.setRequestMethod("GET");
-
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
-
-	}
 	
 	
-	// HTTP POST request
 		private void sendPost() throws Exception {
 
 			String url = "http://challenge.code2040.org/api/register";
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-			//add reuqest header
+			
 			con.setRequestMethod("POST");
 			con.setRequestProperty("User-Agent", USER_AGENT);
-			//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+			
 
 			String urlParameters = "token=625e546a20c2f6d4b7dabe8be841a23e&github=github.com/axeljeremy7/chal1";
 
-			// Send post request
+			
 			con.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 			wr.writeBytes(urlParameters);
@@ -79,9 +46,7 @@ public class Stepmain {
 			wr.close();
 
 			int responseCode = con.getResponseCode();
-			System.out.println("\nSending 'POST' request to URL : " + url);
-			System.out.println("Post parameters : " + urlParameters);
-			System.out.println("Response Code : " + responseCode);
+			
 
 			BufferedReader in = new BufferedReader(
 			        new InputStreamReader(con.getInputStream()));
@@ -93,7 +58,7 @@ public class Stepmain {
 			}
 			in.close();
 
-			//print result
+
 			System.out.println(response.toString());
 
 		}
